ea652751273923377e2df414853d623c06663fe0
stephjac000 <stephjac000@warren.k12.in.us>
2026-02-02 09:43:26 -0500

    2/2 commit

--- app.js
+++ app.js
@@ -12,7 +12,36 @@ const nameSpan = document.getElementById("studentName");
 nameSpan.innerText = name;
 
 let luckyInput = prompt("Pick a lucky number. (1-100)")
-let luckyNumber = +luckyInput;
+
+function fixNaN(){
+    if (isNaN(parseInt(luckyInput))){
+        return "Not A Number";
+    }else{
+        return parseInt(luckyInput);
+    }
+}
+
+let luckyNumber = fixNaN(luckyInput);
 
 const luckyNumberSpan = document.getElementById("luckyNumber");
 luckyNumberSpan.innerText = luckyNumber.toString();
+
+const facts = [name, luckyNumber]
+
+// new child
+const ulElement = document.createElement("ul");
+
+const liElement0 = document.createElement("li");
+liElement0.innerText = name
+const liElement1 = document.createElement("li");
+liElement1.innerText = luckyNumber;
+ulElement.appendChild(liElement0);
+ulElement.appendChild(liElement1);
+
+// parent
+const body = document.getElementById("content");
+
+// last child
+const my_script = document.getElementById("my_script");
+
+body.insertBefore(ulElement, my_script);
